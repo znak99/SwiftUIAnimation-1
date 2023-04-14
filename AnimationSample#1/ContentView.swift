73dac7781c884fd54b5a51de35b37c714369a845
znak99 <xhahekcldhkznak0113@gmail.com>
2023-04-14 11:52:18 +0900

    Simple Animation #1

--- AnimationSample#1/ContentView.swift
+++ AnimationSample#1/ContentView.swift
@@ -8,15 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var scale: CGFloat = 1.0
+
+        var body: some View {
+            Circle()
+                .frame(width: 100, height: 100)
+                .scaleEffect(scale)
+                .animation(.easeInOut(duration: 1), value: scale)
+                .onAppear {
+                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                        withAnimation {
+                            scale = (scale == 1) ? 2 : 1
+                        }
+                    }
+                }
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
