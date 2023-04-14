faa261a5398a8864d963fef5bc5909f18af3d8cd
znak99 <xhahekcldhkznak0113@gmail.com>
2023-04-14 11:16:23 +0900

    Initial Commit

---
+++ AnimationSample#1/AnimationSample_1App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSample_1App.swift
+//  AnimationSample#1
+//
+//  Created by cmStudent on 2023/04/14.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSample_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationSample#1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimationSample#1
+//
+//  Created by cmStudent on 2023/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
